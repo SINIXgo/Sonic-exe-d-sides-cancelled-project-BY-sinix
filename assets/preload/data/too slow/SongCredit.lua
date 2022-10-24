7464f4abad4b8611b0528bf23b328f9bef36a4b7
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-10-24 00:56:17 -0300

    Add files via upload

---
+++ assets/preload/data/too slow/SongCredit.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Created By Philiplol", 0, 10, 680);
+    setTextSize('watermark', 14);
+    addLuaText('watermark');
+end
---
+++ assets/preload/data/too slow/windowname.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': ROUND 1")
+end
+
+function onDestroy()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': ROUND 1")
+end
