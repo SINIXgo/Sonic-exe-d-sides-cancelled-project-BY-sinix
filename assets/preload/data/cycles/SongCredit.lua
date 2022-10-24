ed2116d749a5141ad9f17984cee389c6a59c5539
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-10-23 23:48:31 -0300

    Add files via upload

---
+++ assets/preload/data/cycles/SongCredit.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Created By canofspaghettios", 0, 10, 680);
+    setTextSize('watermark', 14);
+    addLuaText('watermark');
+end
---
+++ assets/preload/data/cycles/windowname.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': Your Soul is Mine")
+end
+
+function onDestroy()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': Your Soul is Mine")
+end
