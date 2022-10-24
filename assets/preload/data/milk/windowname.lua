12041f88abd7526b60f9b73dc182aaa4f57d8308
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-10-24 00:59:23 -0300

    Add files via upload

---
+++ assets/preload/data/milk/windowname.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': JELLY TIME")
+end
+
+function onDestroy()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': Psych Engine")
+end
