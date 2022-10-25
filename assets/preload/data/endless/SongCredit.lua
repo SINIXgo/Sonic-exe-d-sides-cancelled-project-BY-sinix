e96d8d89b8223edcd2157afd2e28429aeac0e45d
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2022-10-25 11:00:03 -0300

    Add files via upload

---
+++ assets/preload/data/endless/SongCredit.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Created By kiwiquest", 0, 10, 680);
+    setTextSize('watermark', 14);
+    addLuaText('watermark');
+end
---
+++ assets/preload/data/endless/windowname.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': YOU NEVER STOOD A CHANCE")
+end
+
+function onDestroy()
+    setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': YOU NEVER STOOD A CHANCE")
+end
